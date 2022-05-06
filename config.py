74192b9a0c7fff5376016ecf7a54d25fd235744d
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-05-05 23:09:53 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     API_ID = "9049867"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
     API_HASH = "972c0f2e94973b5ffa9a89cb34e5671b" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "5100305269:AAGKc3IQ-scxhirZagPMY2p2Ivejvj9R5BQ" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    BOT_TOKEN = "5100305269:AAEHxCE1z9jCFZl4b0-yoRfVfojKBRKSL0Q" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
