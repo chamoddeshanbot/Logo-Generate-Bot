162732ee631a653114e96ed841a112295556caa9
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-05-06 10:03:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Logo-Generate-Bot
+# Logo-Generate-bot
+
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/chamoddeshanbot/Logo--Generate-bot.git)
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    API_ID = "9049867"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "972c0f2e94973b5ffa9a89cb34e5671b" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5100305269:AAGKc3IQ-scxhirZagPMY2p2Ivejvj9R5BQ" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
