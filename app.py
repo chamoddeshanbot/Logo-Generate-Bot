4bdee7e0c59fa84eca485c576fe9a1eb19030431
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-05-05 21:38:16 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from pyrogram import Client
+from config import *
+
+app = Client(
+  "bot",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=BOT_TOKEN
+)
+
+print("[INFO]: STARTING BOT...")
+app.start()
