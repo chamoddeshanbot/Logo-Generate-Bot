8cd2c584f3110c0f7c03d2d8c1a9ceae58532535
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-05-06 22:22:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -522,24 +522,25 @@ async def help(_,query):
   await query.answer(f"🏖 Bot Help 🏖")
   await query.message.edit(HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="<<< Back", callback_data="start")]]))
 
-@app.on_callback_query(filters.regex("start"))
-async def start(_,query):
-  await query.message.delete()
-  await query.answer(f"🏖 Bot Menu 🏖")
-  await start(app, query.message)
-
-@app.on_callback_query(filters.regex("ib"))
-async def ib(_,query):
-  await query.message.delete()
-  await query.answer(f"🛠 Send Inbox 🛠")
-  await ib(app, query.message)
 
 @app.on_callback_query()
 async def button(app, update):
       cb_data = update.data
       if "ib" in cb_data:
         await update.message.delete()
+        await update.answer(f"🛠 Send Inbox 🛠")
         await ib(app, update.message)
+      elif "start" in cb_data:
+        await update.message.delete()
+        await update.answer(f"🏖 Bot Menu 🏖")
+        await start(app, update.message)
+
+@app.on_callback_query()
+async def button(app, update):
+      cb_data = update.data
+      if "ibuyy" in cb_data:
+        await update.message.delete()
+        await ibee(app, update.message)
      
 
 @app.on_inline_query()
