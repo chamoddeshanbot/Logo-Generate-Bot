90115a4167ca6431770f2bbaeb15861b31bbfe3b
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-05-09 08:55:00 -0700

    Update logo.py

--- logo.py
+++ logo.py
@@ -1,8 +1,41 @@
 from typing import Optional
-import aiohttp
+import aiohttp, random
 
 LOGO_API_URL1 = "https://techzbotsapi.herokuapp.com/logo?text="
 LOGO_API_URL2 = "https://techzbotsapi.herokuapp.com/logo?square=true&text="
+
+
+WALL_API = "https://techzbotsapi.herokuapp.com/wall?query="
+UNSPLASH_API = "https://techzbotsapi.herokuapp.com/unsplash?query="
+
+
+session = aiohttp.ClientSession()
+
+async def get_wallpapers(query: str):  
+  try:
+    url = WALL_API + query  
+    resp = await session.get(url)
+    json = await resp.json()
+    images = json["images"]
+    if len(images) == 0:
+      return "nonee " + "Can't find the wallpaper you are trying to search..."
+    random.shuffle(images)
+  except Exception as e:
+    return "error " + str(e)      
+  return images
+
+async def get_unsplash(query: str):  
+  try:
+    url = UNSPLASH_API + query  
+    resp = await session.get(url)
+    json = await resp.json()
+    images = json["images"]
+    if len(images) == 0:
+      return "nonee " + "Can't find the wallpaper you are trying to search..."
+    random.shuffle(images)
+  except Exception as e:
+    return "error " + str(e)      
+  return images
 
 async def generate_logo(text: str, square: Optional[bool] = False ):
   "To Create Logos. text = What you want to write on the logo. square = If You Want Square Logo Or Not. Returns Telgraph Image Url"
