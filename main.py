847c0ebb39abe19e71c6b993cd348aa170ee9c91
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-05-15 01:33:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -659,19 +659,19 @@ async def help(_,query):
 
 @app.on_callback_query(filters.regex("ib"))
 async def ib(_,query):
-  try:
-      message = query.message
-      callback_data = query.data.strip()
-      callback_request = callback_data.split(None, 1)[1]
-      user_id = callback_request.split("|")
-    if query.from_user.id != int(user_id):
-      return await CallbackQuery.answer("You're not allowed to use this.")
-      await query.answer(f"🏖 Send Inbox 🏖")
-      await query.message.copy(chat_id=query.from_user.id)
-      await query.message.delete()
-      await query.message.reply(IB,
-          reply_markup=InlineKeyboardMarkup(
-              [[InlineKeyboardButton("✖️ close ✖️", callback_data="close}")]]))
+    try:
+        message = query.message
+        callback_data = query.data.strip()
+        callback_request = callback_data.split(None, 1)[1]
+        user_id = callback_request.split("|")
+     if query.from_user.id != int(user_id):
+        await CallbackQuery.answer("You're not allowed to use this.")
+        await query.answer(f"🏖 Send Inbox 🏖")
+        await query.message.copy(chat_id=query.from_user.id)
+        await query.message.delete()
+        await query.message.reply(IB,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("✖️ close ✖️", callback_data="close}")]]))
     
 
     
